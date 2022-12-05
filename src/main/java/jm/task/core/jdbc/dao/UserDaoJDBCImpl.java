a6a7f54635b40937887d5476d1ce6b96f531f4d4
Rodion <rodion1ivanov1@mail.ru>
2022-12-05 03:08:13 +0300

    Second

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,10 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,38 +15,33 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void createUsersTable() throws SQLException {
         Connection connection = Util.getConnection();
-        Statement statement = connection.createStatement();
-        try {
-            statement.execute("create table users(id int not null auto_increment primary key, name varchar(45), lastName varchar(45), age int)");
-        } catch (SQLException e) {}
+        PreparedStatement statement = (PreparedStatement) connection.createStatement();
+        statement.execute("create table if not exists users(id int not null auto_increment primary key, name varchar(45), lastName varchar(45), age int)");
     }
 
     public void dropUsersTable() throws SQLException {
         Connection connection = Util.getConnection();
-        Statement statement = connection.createStatement();
-        try {
-            statement.execute("DROP TABLE users");
-        } catch (SQLException e) {}
-
+        PreparedStatement statement = (PreparedStatement) connection.createStatement();
+        statement.execute("drop table if exists users;");
     }
 
     public void saveUser(String name, String lastName, byte age) throws SQLException {
         Connection connection = Util.getConnection();
-        Statement statement = connection.createStatement();
+        PreparedStatement statement = (PreparedStatement) connection.createStatement();
         statement.execute("INSERT INTO users (name, lastname, age) VALUES('" + name + "', '" + lastName +"', "+ age + ")");
         System.out.println("User with name " + name+ " has been added to the database");
     }
 
     public void removeUserById(long id) throws SQLException {
         Connection connection = Util.getConnection();
-        Statement statement = connection.createStatement();
+        PreparedStatement statement = (PreparedStatement) connection.createStatement();
         statement.executeUpdate("delete from users where id="+id);
     }
 
     public List<User> getAllUsers() throws SQLException {
         List<User> userList = new ArrayList<User>();
         Connection connection = Util.getConnection();
-        Statement statement = connection.createStatement();
+        PreparedStatement statement = (PreparedStatement) connection.createStatement();
         ResultSet resultSet = statement.executeQuery("select * from users");
         while (resultSet.next()) {
             User user = new User();
@@ -65,7 +57,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void cleanUsersTable() throws SQLException {
         Connection connection = Util.getConnection();
-        Statement statement = connection.createStatement();
+        PreparedStatement statement = (PreparedStatement) connection.createStatement();
         statement.executeUpdate("delete from users");
     }
 }
